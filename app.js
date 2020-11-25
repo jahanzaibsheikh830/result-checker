6846081a4fe07c588bb7122f8e318bdb7504e0d8
Jahanzaib Sheikh <53260475+jahanzaibsheikh830@users.noreply.github.com>
2020-11-25 16:10:21 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var obtMarks = document.getElementById('om');
 var result = document.getElementById('result');
 
 function check(){
-    var percentage = (obtMarks.value / totalMarks.value) * 100;
-    console.log(parseInt( percentage))
+    var percentage = parseFloat((obtMarks.value / totalMarks.value) * 100);
     if(percentage >= 80 && percentage <= 100){
         result.innerHTML = "Your Grade Is A+ " +"and You Got " + percentage + "%";
     }
-    else if(percentage >= 70 && percentage <= 79){
+    else if(percentage >= 70 && percentage <= 80){
         result.innerHTML = "Your Grade Is A " +"and You Got " + percentage + "%";
     }
-    else if(percentage >= 60 && percentage <= 69){
+    else if(percentage >= 60 && percentage <= 70){
         result.innerHTML = "Your Grade Is B " +"and You Got " + percentage + "%";
     }
     else{
